47f7910b182a6acf275c37723bedeec30297fb49
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-07 19:18:08 +0200

    adds some fix

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,10 +1,12 @@
 const menuButtonEl = document.querySelectorAll('.header__button');
 const menuEl = document.querySelector('.mobile-menu');
 const menuLinksEl = document.querySelectorAll('.link');
+const heroContainerEl = document.querySelector('.hero__container');
 
 const toggleMenu = () => {
   menuEl.classList.toggle('is-open');
   document.body.classList.toggle('no-scroll');
+  heroContainerEl.classList.toggle('hidden');
   [menuButtonEl[0], menuButtonEl[1]].forEach(button => button.classList.toggle('hidden'));
   menuLinksEl[0].style.zIndex = document.body.classList.contains('no-scroll') ? 1 : '';
 };
