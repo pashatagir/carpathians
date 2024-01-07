380ea03dd4a0f62bf1df4a169b8903ce8e2b8801
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-07 13:19:47 +0200

    adds mobile-menu

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
 import './sass/index.scss';
 import './js/initSwiper';
+import './js/mobileMenu';
---
+++ src/js/mobileMenu.js
@@ -0,0 +1,25 @@
+const menuButtonEl = document.querySelectorAll('.header__button');
+const menuEl = document.querySelector('.mobile-menu');
+const menuLinksEl = document.querySelectorAll('.link');
+
+menuButtonEl.forEach(item => {
+  item.addEventListener('click', e => {
+    menuEl.classList.toggle('is-open');
+    document.body.classList.toggle('no-scroll');
+    menuLinksEl[0].style.zIndex = 1;
+    menuButtonEl[0].classList.toggle('hidden');
+    menuButtonEl[1].classList.toggle('hidden');
+  });
+});
+
+menuLinksEl.forEach(item =>
+  item.addEventListener('click', e => {
+    if (!document.body.classList.contains('no-scroll')) {
+      return;
+    }
+    menuEl.classList.remove('is-open');
+    document.body.classList.remove('no-scroll');
+    menuButtonEl[0].classList.toggle('hidden');
+    menuButtonEl[1].classList.toggle('hidden');
+  })
+);
