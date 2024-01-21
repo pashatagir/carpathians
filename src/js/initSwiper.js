f8c46c4a52a1381b4f0eba22fdd17b45086c470c
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-21 16:46:30 +0200

    adds some images and styles for desktop version

--- src/js/initSwiper.js
+++ src/js/initSwiper.js
@@ -7,7 +7,6 @@ const swiper = new Swiper('.swiper__gallery', {
   slidesPerView: 'auto',
   spaceBetween: 15,
   centeredSlides: true,
-
   breakpoints: {
     768: {
       slidesPerView: 2,
@@ -15,12 +14,34 @@ const swiper = new Swiper('.swiper__gallery', {
       centeredSlides: false,
     },
   },
-
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
 });
+
+const swiperTours = new Swiper('.swiper__tours', {
+  modules: [Navigation, Pagination],
+  slidesPerView: 'auto',
+  spaceBetween: 15,
+  centeredSlides: true,
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+      centeredSlides: false,
+    },
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+if (window.innerWidth >= 1440) {
+  swiperTours.disable();
+  swiperTours.destroy(false, true);
+}
 
 const swiperTour = new Swiper('.swiper__modal', {
   modules: [Navigation, Pagination],
