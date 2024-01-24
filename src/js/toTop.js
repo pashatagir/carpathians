1f5134f5c20e99e0328f247ec993402c2bf3fca5
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-24 16:27:15 +0200

    adds fix height mobile menu on iOs

--- src/js/toTop.js
+++ src/js/toTop.js
@@ -1,11 +1,9 @@
-// import smoothscroll from 'smoothscroll-polyfill';
 import { refs } from './refs';
 const { backHomeEl, anchorEls } = refs;
 
 anchorEls.forEach(anchor => {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
-    // smoothscroll.polyfill();
     document.querySelector(this.getAttribute('href')).scrollIntoView({ behavior: 'smooth' });
   });
 });
