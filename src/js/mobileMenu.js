9d5bf94742632c9c6b13b2a7fa32aafcfd7da5f7
Pavlo Bezsinniy <108672605+pashatagir@users.noreply.github.com>
2024-01-24 14:33:59 +0200

    adds some fix

--- src/js/mobileMenu.js
+++ src/js/mobileMenu.js
@@ -1,17 +1,9 @@
 import { refs } from './refs';
-const { menuButtonEls, menuLinkEls, menuEl, heroContainerEl } = refs;
+const { menuButtonEls, menuLinkEls, menuEl } = refs;
 
 const toggleMenu = () => {
   menuEl.classList.toggle('is-open');
   document.body.classList.toggle('no-scroll');
-  heroContainerEl.classList.toggle('visually-hidden');
-  [menuButtonEls[0], menuButtonEls[1]].forEach(button =>
-    button.classList.toggle('visually-hidden')
-  );
-  menuLinkEls[0].style.zIndex = document.body.classList.contains('no-scroll') ? 1 : '';
-  menuLinkEls[0].style.transform = document.body.classList.contains('no-scroll')
-    ? 'translateX(62px)'
-    : '';
 };
 
 menuButtonEls.forEach(item => item.addEventListener('click', toggleMenu));
